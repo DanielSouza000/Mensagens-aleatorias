2ccd6bdcbe2ea0a1dd2a67608e82742a8549fe4c
DanielSouza000 <120104766+DanielSouza000@users.noreply.github.com>
2024-05-06 21:12:38 -0300

    Retornando a library antiga
    Problema que estava tendo ao conectar ao banco de dados com a library mysql-connector foi resolvido ao adicionar novos parâmetros. Logo estou retornando ao utilizar
    esta devido a erros ao selecionar (Select) dados recentemente adicionados ao banco de dados utilizando a library antiga, cuja esta não tem.

--- run.py
+++ run.py
@@ -1,47 +1,54 @@
+import mysql.connector
 import random
 import os
-import pymysql
-import pymysql.cursors
 
 timeout = 10
-connection = pymysql.connect(
-    charset="utf8mb4",
-    connect_timeout=timeout,
-    cursorclass=pymysql.cursors.DictCursor,
-    db="mensagensaleatorias",
-    host="mysql-1b8e19e2-mensagensaleatorias.d.aivencloud.com",
-    password="xVN<znE38HR9]-MJsrBU(W",
-    read_timeout=timeout,
-    port=13649,
-    user="newuser",
-    write_timeout=timeout,
-)
+mydb = mysql.connector.connect(
+  charset="utf8mb4",
+  host="mysql-1b8e19e2-mensagensaleatorias.d.aivencloud.com",
+  user="newuser",
+  password="xVN<znE38HR9]-MJsrBU(W",
+  database="mensagensaleatorias",
+  port=13649)
+
+mycursor = mydb.cursor()
+
 def enviar():
-    with connection:
-        with connection.cursor() as cursor:
-            sql = "INSERT INTO `mensagens` (`nome`, `mensagem`) VALUES (%s, %s)"
-            nome = input("Escreva seu nome: ")
-            mensagem = input("Escreva sua mensagem: ")
-            val = (nome, mensagem)
-            if nome.strip() == "" or mensagem.strip() == "":
-                check = input("Você não escreveu um nome ou uma mensagem.")
-                check = ""
-            elif len(nome) > 40 or len(mensagem) > 200:
-                check = input("O nome excedeu o limite (40), ou a mensagem excedeu o limite (200)")
-                check = ""
-            else:
-                cursor.execute(sql, val)
-                connection.commit()
+  sql = "INSERT INTO mensagens (nome, mensagem) VALUES (%s, %s)"
+  nome = input("Escreva seu nome: ")
+  mensagem = input("Escreva sua mensagem: ")
+  val = (nome, mensagem)
+
+
+  if nome.strip() == "" or mensagem.strip() == "":
+    check = input("Você não escreveu um nome ou uma mensagem.")
+    check = ""
+  elif len(nome) > 40 or len(mensagem) > 200:
+    check = input("O nome excedeu o limite (40), ou a mensagem excedeu o limite (200)")
+    check = ""
+  else:
+    mycursor.execute(sql, val)
+
+    mydb.commit()
+
+    print("Sua mensagem foi enviada")
+    check = input("Pressione Enter para continuar")
+    check = ""
 
 def ver():
-    with connection.cursor() as cursor:
-        cursor = connection.cursor(pymysql.cursors.DictCursor)
-        cursor.execute("SELECT nome, mensagem FROM mensagens")
-        myresult = cursor.fetchall()
-        random_int = random.randint(0, len(myresult)-1)
-        print("\"",myresult[random_int]["nome"],"\"\n","-",myresult[random_int]["mensagem"],"\n")
-        check = input("Pressione Enter para continuar")
-        check = ""
+  mycursor.execute("SELECT nome, mensagem FROM mensagens")
+
+  myresult = mycursor.fetchall()
+
+  random_int = random.randint(0, len(myresult)-1)
+
+  nome_aleatorio = myresult[random_int][0]
+  men_aleatorio = myresult[random_int][1]
+
+  print("\"",men_aleatorio,"\"\n","-",nome_aleatorio,"\n")
+  check = input("Pressione Enter para continuar")
+  check = ""
+
 
 while True:
   os.system('cls')
